1c3d3b096c1583d0dd1f2313453da6c6b0ea72c4
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-11 00:44:49 +0530

    Questions

--- src/Questions.java
+++ src/Questions.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Questions {
     public static void main(String[] args) {
-        int[] arr = {4,3,2,6,2,1};
-        findDuplicate(arr);
-        System.out.println(findDuplicate(arr));
+        int[] arr = {4,3,2,7,8,2,3,1};
+        findDuplicates(arr);
+        System.out.println(findDuplicates(arr));
         System.out.println(Arrays.toString(arr));
     }
     //Q1    // https://leetcode.com/problems/missing-number/
@@ -84,6 +84,28 @@ public class Questions {
         return  -1;
     }
 
+    //Q4    //https://leetcode.com/problems/find-all-duplicates-in-an-array/
+    public static List<Integer> findDuplicates(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        //FIRST WE WILL DO CYCLE SORT
+        int i = 0;
+        while( i < arr.length){
+            int correctIndex = arr[i] - 1 ;
+            if(correctIndex < arr.length && arr[i] != arr[correctIndex] ){
+                swap(arr, i ,correctIndex);
+            }
+            else {
+                i++;
+            }
+        }
+        // ADD THE DUPLICATING ELEMENTS IN NEW ARRAYLIST
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index + 1){
+                ans.add(arr[index]);
+            }
+        }
+        return ans;
+    }
 
     static void swap(int[] arr, int ele1, int ele2){
         int temp = arr[ele1];
