2c4e125c9a98473703c71fa56204947c025f9e25
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-11 00:23:43 +0530

    :|

--- src/CycleSort.java
+++ src/CycleSort.java
@@ -1,2 +1,33 @@
+import java.util.Arrays;
+
 public class CycleSort {
+    // When given numbers from 1 to N, use CYCLE SORT
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,1,2};
+        cycleSort(arr);
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = i + 1;
+//
+//        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cycleSort(int[] arr){
+        int i = 0;
+        while( i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if(arr[i] != arr[correctIndex]){
+                swap(arr, i ,correctIndex);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr, int ele1, int ele2){
+        int temp = arr[ele1];
+        arr[ele1] = arr[ele2];
+        arr[ele2] = temp;
+    }
 }
---
+++ src/Questions.java
@@ -0,0 +1,93 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Questions {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,6,2,1};
+        findDuplicate(arr);
+        System.out.println(findDuplicate(arr));
+        System.out.println(Arrays.toString(arr));
+    }
+    //Q1    // https://leetcode.com/problems/missing-number/
+    public static int missingNumber(int[] arr){
+        //FIRST WE WILL DO CYCLE SORT
+        int i = 0;
+        while( i < arr.length){
+            int correctIndex = arr[i] ;
+            if(correctIndex < arr.length && arr[i] != arr[correctIndex] ){
+                swap(arr, i ,correctIndex);
+            }
+            else {
+                i++;
+            }
+        }
+        // THEN SEARCH FOR FIRST MISSING NUMBER
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index){
+                return index;
+            }
+        }
+        return arr.length;
+    }
+
+    //Q2    //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+    public static ArrayList<Integer> findDisappearedNumbers(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        //FIRST WE WILL DO CYCLE SORT
+        int i = 0;
+        while( i < arr.length){
+            int correctIndex = arr[i] - 1 ;
+            if(correctIndex < arr.length && arr[i] != arr[correctIndex] ){
+                swap(arr, i ,correctIndex);
+            }
+            else {
+                i++;
+            }
+        }
+        // ADD THE MISSING ELEMENTS IN NEW ARRAYLIST
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index + 1){
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+
+    //Q3    //https://leetcode.com/problems/find-the-duplicate-number/
+    public static int findDuplicate(int[] arr) {
+        //FIRST WE WILL DO CYCLE SORT
+        int i = 0;
+        while( i < arr.length){
+            // We check that element is equal to {index + 1} or not
+            // If it is then go ahead
+            // If it is not then find the correct position for that element
+            if(arr[i] != i + 1){
+                int correctIndex = arr[i] - 1 ;
+                // After finding the correct position for the elements
+                // now check whether the element is equal to the element at the correct position or not
+                // if not then swap
+                // if yes then it is duplicate
+                if(correctIndex < arr.length && arr[i] != arr[correctIndex] ){
+                    swap(arr, i ,correctIndex);
+                }
+                else {
+                    return arr[i];
+                }
+            }
+            else {
+                i++;
+            }
+        }
+
+        return  -1;
+    }
+
+
+    static void swap(int[] arr, int ele1, int ele2){
+        int temp = arr[ele1];
+        arr[ele1] = arr[ele2];
+        arr[ele2] = temp;
+    }
+}
