0a679cdc7cccc5d47d2a1b85845715ca44c7b9d0
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-13 16:26:31 +0530

    done

--- src/Questions.java
+++ src/Questions.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Questions {
     public static void main(String[] args) {
-        int[] arr = {4,3,2,7,8,2,3,1};
-        findDuplicates(arr);
-        System.out.println(findDuplicates(arr));
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {0,1,2};
+        System.out.println(firstMissingPositive(arr));
+
+
     }
     //Q1    // https://leetcode.com/problems/missing-number/
     public static int missingNumber(int[] arr){
@@ -56,7 +56,7 @@ public class Questions {
     }
 
     //Q3    //https://leetcode.com/problems/find-the-duplicate-number/
-    public static int findDuplicate(int[] arr) {
+    public static int findDuplicate(int[] arr)  {
         //FIRST WE WILL DO CYCLE SORT
         int i = 0;
         while( i < arr.length){
@@ -106,6 +106,69 @@ public class Questions {
         }
         return ans;
     }
+    
+    //Q5    //https://leetcode.com/problems/set-mismatch/
+    public static int[] findErrorNums(int[] arr) {
+        int ans[] = {-1,-1};
+        //First we sort
+        int i = 0;
+        while( i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if(arr[i] != arr[correctIndex]){
+                swap(arr, i ,correctIndex);
+            }
+            else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            int correctNumber = j + 1;
+            if(arr[j] != correctNumber){
+                ans[0] = arr[j];    // Duplicate number
+                ans[1] = j + 1;// Missing Number
+            }
+            break;
+        }
+        return ans;
+    }
+
+    //Q6    //https://leetcode.com/problems/first-missing-positive/
+    public static int firstMissingPositive(int[] nums) {
+        //FIRST BUBBLE SORT
+        boolean swapped;
+        // run the steps n - 1 times
+        for (int i = 0; i < nums.length - 1; i++) {
+            swapped = false;
+            // for each step. max item will come at the last respective index
+            for (int j = 1; j <= nums.length - i -1 ; j++) {
+                // swap if item is smaller than previous item
+                if (nums[j] < nums[j-1]){
+                    //swap
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            // if there is no swap for i = 0, then array is sorted
+            if(! swapped) {
+                break;
+            }
+        }
+        int ans = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != ans){
+                ans = ans;
+            }
+            else{
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+
 
     static void swap(int[] arr, int ele1, int ele2){
         int temp = arr[ele1];
